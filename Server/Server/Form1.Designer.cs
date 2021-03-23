788e07de17895142cda179e09244f66c4dc89732
LirimM <34185066+LirimM@users.noreply.github.com>
2021-03-23 20:41:23 +0100

    Add files via upload

---
+++ Server/Server/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ServerIPtextBox = new System.Windows.Forms.TextBox();
+            this.ServerPorttextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MessagetextBox = new System.Windows.Forms.TextBox();
+            this.ChatScreentextBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(444, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(444, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SEND";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ServerIPtextBox
+            // 
+            this.ServerIPtextBox.Location = new System.Drawing.Point(52, 33);
+            this.ServerIPtextBox.Name = "ServerIPtextBox";
+            this.ServerIPtextBox.Size = new System.Drawing.Size(159, 20);
+            this.ServerIPtextBox.TabIndex = 2;
+            // 
+            // ServerPorttextBox
+            // 
+            this.ServerPorttextBox.Location = new System.Drawing.Point(261, 33);
+            this.ServerPorttextBox.Name = "ServerPorttextBox";
+            this.ServerPorttextBox.Size = new System.Drawing.Size(159, 20);
+            this.ServerPorttextBox.TabIndex = 3;
+            this.ServerPorttextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PORT:";
+            // 
+            // MessagetextBox
+            // 
+            this.MessagetextBox.Location = new System.Drawing.Point(52, 214);
+            this.MessagetextBox.Multiline = true;
+            this.MessagetextBox.Name = "MessagetextBox";
+            this.MessagetextBox.Size = new System.Drawing.Size(386, 36);
+            this.MessagetextBox.TabIndex = 6;
+            // 
+            // ChatScreentextBox
+            // 
+            this.ChatScreentextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ChatScreentextBox.Location = new System.Drawing.Point(52, 75);
+            this.ChatScreentextBox.Multiline = true;
+            this.ChatScreentextBox.Name = "ChatScreentextBox";
+            this.ChatScreentextBox.ReadOnly = true;
+            this.ChatScreentextBox.Size = new System.Drawing.Size(460, 133);
+            this.ChatScreentextBox.TabIndex = 7;
+            this.ChatScreentextBox.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 275);
+            this.Controls.Add(this.ChatScreentextBox);
+            this.Controls.Add(this.MessagetextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ServerPorttextBox);
+            this.Controls.Add(this.ServerIPtextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox ServerIPtextBox;
+        private System.Windows.Forms.TextBox ServerPorttextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MessagetextBox;
+        private System.Windows.Forms.TextBox ChatScreentextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ Server/Server/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public String receive;
+        public String TextToSend;
+        public Form1()
+        {
+            InitializeComponent();
+
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach (IPAddress address in localIP)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ServerIPtextBox.Text = address.ToString();
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(ServerPorttextBox.Text));
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            STR = new StreamReader(client.GetStream());
+            STW = new StreamWriter(client.GetStream());
+            STW.AutoFlush = true;
+
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker2.WorkerSupportsCancellation = true;
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    receive = STR.ReadLine();
+                    if (receive != null)
+                    {
+                        this.ChatScreentextBox.Invoke(new MethodInvoker(delegate ()
+                        {
+                            ChatScreentextBox.AppendText("\nClient:" + Dekripto(receive));
+                            ChatScreentextBox.AppendText(Environment.NewLine);
+                        }));
+                        receive = "";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                if (TextToSend != null)
+                {
+                    TextToSend = Enkripto(TextToSend);
+                    STW.WriteLine(TextToSend);
+                    this.ChatScreentextBox.Invoke(new MethodInvoker(delegate ()
+                    {
+                        ChatScreentextBox.AppendText("\nServer:" + TextToSend);
+                        ChatScreentextBox.AppendText(Environment.NewLine);
+                    }));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sending failed");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessagetextBox.Text != "")
+            {
+                TextToSend = MessagetextBox.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            MessagetextBox.Text = "";
+        }
+
+        public string Enkripto(string TextToSend)
+        {
+            byte[] Celesi = Encoding.UTF8.GetBytes("12365478");
+            DESCryptoServiceProvider objDES = new DESCryptoServiceProvider();
+
+            byte[] bytePlainTexti = Encoding.UTF8.GetBytes(TextToSend);
+
+            objDES.Key = Celesi;
+            objDES.Padding = PaddingMode.Zeros;
+            objDES.Mode = CipherMode.CBC;
+            objDES.IV = Encoding.UTF8.GetBytes("11223344");
+
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, objDES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            cs.Write(bytePlainTexti, 0, bytePlainTexti.Length);
+            cs.Close();
+
+            byte[] byteCiphertexti = ms.ToArray();
+            return Convert.ToBase64String(byteCiphertexti);
+
+
+
+        }
+
+        public string Dekripto(string cyphertext)
+        {
+            byte[] celesi = Encoding.UTF8.GetBytes("12365478");
+
+            DESCryptoServiceProvider objDES = new DESCryptoServiceProvider();
+
+            objDES.Key = celesi;
+            objDES.Padding = PaddingMode.Zeros;
+            objDES.IV = Encoding.UTF8.GetBytes("11223344");
+            objDES.Mode = CipherMode.CBC;
+            cyphertext = cyphertext.Replace(" ", "+");
+            int mod4 = cyphertext.Length % 4;
+            if (mod4 > 0)
+            {
+                cyphertext += new string('=', 4 - mod4);
+            }
+            byte[] ciphertexti = Convert.FromBase64String(cyphertext);
+            MemoryStream ms = new MemoryStream(ciphertexti);
+            CryptoStream cs = new CryptoStream(ms, objDES.CreateDecryptor(), CryptoStreamMode.Read);
+            byte[] bytetextiDekriptuar = new byte[ms.Length];
+            cs.Read(bytetextiDekriptuar, 0, bytetextiDekriptuar.Length);
+            cs.Close();
+
+            return Encoding.UTF8.GetString(bytetextiDekriptuar);
+        }
+    }
+}
